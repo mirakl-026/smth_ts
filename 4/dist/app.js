3d0211a1aa35a6d28a81484fcb2da34d64eafdbe
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-12-09 19:17:44 +0300

    4 - intersection types

---
+++ 4/dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+// intersection types
+// позволяют комбинировать разные типы
+const e1 = {
+    name: "John",
+    privileges: ["work", "not work"],
+    startDate: new Date()
+};
+console.log(e1);
+//const a : Universal = "aaaa";   // нет
+const b = 10; // нормально
--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,2 +1,34 @@
-// advanced
-console.log("Hi");
+// intersection types
+// позволяют комбинировать разные типы
+
+// описание типа админ
+type Admin = {
+    name: string;
+    privileges: string[];
+}
+
+// описание типа сотррудник
+type Employee = {
+    name: string;
+    startDate: Date;
+}
+
+// комбинированный тип админа и сотрудника
+type ElevatedEmployee = Admin & Employee;   // в таком случае буду все поля, без повторов [name, privileges, startDate]
+
+const e1 : ElevatedEmployee = {
+    name: "John",
+    privileges: ["work", "not work"],
+    startDate: new Date()
+}
+
+console.log(e1);
+
+// при использовании с Union получается так
+type Combinable = string | number;
+type Numerical = number | boolean;
+type Universal = Combinable & Numerical;    // тут пересечение - number
+
+//const a : Universal = "aaaa";   // нет
+const b : Universal = 10;   // нормально
+
