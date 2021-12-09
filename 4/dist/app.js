e299bd96e02bc535a10f25ba04089b3ff33175c1
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-12-09 19:38:56 +0300

    4 - type guards

--- 4/dist/app.js
+++ 4/dist/app.js
@@ -9,3 +9,64 @@ const e1 = {
 console.log(e1);
 //const a : Universal = "aaaa";   // нет
 const b = 10; // нормально
+// Type Guards
+// вспоминаем работу с юнионами
+function add(n1, n2) {
+    // обязаны проверить на типы - это и есть type Guard вообще-то с использованием typeof
+    if (typeof n1 === "string" || typeof n2 === "string") {
+        return n1.toString() + n2.toString();
+    }
+    return n1 + n2;
+}
+console.log(add("2", 3));
+console.log(add(1, 4));
+function printEmployeeInformation(emp) {
+    console.log("emp name: " + emp.name); // здесь нет проблем, т.к. имя есть и у employee и у админа
+    //if (typeof emp === "object")    // type guard через typeof здесь не сработает, т.к. всегда object
+    // поэтому надо проверять прямо на то, что нужно и через JS (так как проверки по правилам JS делаются):
+    if ("privileges" in emp) {
+        console.log("emp privileges: " + emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("emp startDate: " + emp.startDate);
+    }
+}
+const ue1 = {
+    name: "Hazan",
+    startDate: new Date()
+};
+const ue2 = {
+    name: "Makesh",
+    privileges: ["draw", "cook"]
+};
+printEmployeeInformation(ue1);
+printEmployeeInformation(ue2);
+// Type Guard для классов:
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving truck...");
+    }
+    cargoLoad(amount) {
+        console.log("cargo load... " + amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(v) {
+    v.drive();
+    // можно так-же как с типами
+    // if ("cargoLoad" in v) {
+    //     v.cargoLoad(1000);
+    // }
+    // но лучше как с классами (но нельзя с интерфейсами, потому как после компиляции их не существует)
+    if (v instanceof Truck) {
+        v.cargoLoad(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- 4/src/app.ts
+++ 4/src/app.ts
@@ -32,3 +32,80 @@ type Universal = Combinable & Numerical;    // тут пересечение - n
 //const a : Universal = "aaaa";   // нет
 const b : Universal = 10;   // нормально
 
+// Type Guards
+// вспоминаем работу с юнионами
+function add(n1: Combinable, n2: Combinable) {
+    // обязаны проверить на типы - это и есть type Guard вообще-то с использованием typeof
+    if (typeof n1 === "string" || typeof n2 === "string") {
+        return n1.toString() + n2.toString();
+    }
+    return n1 + n2;
+}
+
+console.log(add("2", 3));
+console.log(add(1,4));
+
+// а вот с кастомными типами проверка на typeof не получится, так как все они object
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+    console.log("emp name: " + emp.name);   // здесь нет проблем, т.к. имя есть и у employee и у админа
+    //if (typeof emp === "object")    // type guard через typeof здесь не сработает, т.к. всегда object
+    // поэтому надо проверять прямо на то, что нужно и через JS (так как проверки по правилам JS делаются):
+    if ("privileges" in emp) {
+        console.log("emp privileges: " + emp.privileges);
+    }
+
+    if ("startDate" in emp) {
+        console.log("emp startDate: " + emp.startDate);
+    }
+}
+
+const ue1 : UnknownEmployee = {
+    name: "Hazan",
+    startDate: new Date()
+}
+
+const ue2: UnknownEmployee = {
+    name: "Makesh",
+    privileges: ["draw", "cook"]
+}
+
+printEmployeeInformation(ue1);
+printEmployeeInformation(ue2);
+
+// Type Guard для классов:
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+
+class Truck {
+    drive() {
+        console.log("Driving truck...");
+    }
+
+    cargoLoad(amount: number) {
+        console.log("cargo load... " + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+const v1: Vehicle = new Car();
+const v2: Vehicle = new Truck();
+
+function useVehicle(v: Vehicle) {
+    v.drive();
+    // можно так-же как с типами
+    // if ("cargoLoad" in v) {
+    //     v.cargoLoad(1000);
+    // }
+    // но лучше как с классами (но нельзя с интерфейсами, потому как после компиляции их не существует)
+    if (v instanceof Truck) {
+        v.cargoLoad(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
