7f2c7a87b56259181b4f9c7db4da5c001a7ae97f
Evgeniy Bushin <evgeniy.pgm@inbox.ru>
2021-12-10 18:16:14 +0300

    5 - примеры дженериков - массивы и промисы

--- 5/dist/app.js
+++ 5/dist/app.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hi");
+// generics
+// дженерики - обощённые типы, которые могут принимать одинаковую логику к переменным разных типов, пример - массив
+// в JS нет дженериков, чисто TS возможности
+const stringArray1 = [];
+const stringArray2 = []; // и stringArray1 и stringArray2 - массивы строк
+stringArray2[0] = "la-la-la";
+stringArray2[0].split('-');
+// другой пример - промисы:
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("done!");
+    }, 1000);
+});
+// почему важно указывать тип промиса? - чтобы дальше правильно его обработать:
+promise.then((data) => {
+    console.log(data[0] + data[1]); // do
+});
